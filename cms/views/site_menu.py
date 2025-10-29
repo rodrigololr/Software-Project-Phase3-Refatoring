d8465da53ef74836b29d71a1912193a292569afe
Rodrigo Lima <rodrigotenorio2005@gmail.com>
2025-10-29 00:11:41 -0300

    refactor: formatação de código site_menu.py

--- cms/views/site_menu.py
+++ cms/views/site_menu.py
@@ -34,28 +34,33 @@ class SiteMenu(AbstractMenu):
         options: list[MenuOptions] = [
             {"message": "Selecionar posts do site", "function": self._select_post},
         ]
-        
+
         # singleton!
         if AppContext().permission_repo.has_permission(
             self.logged_user, self.selected_site
         ):
             options.extend(
                 [
-                    {"message": "Criar post no site", "function": self._create_site_post},
-                    {"message": "Biblioteca de Mídias", "function": self._media_library_menu},
-                    {"message": "Ver estatísticas do site", "function": self._show_site_analytics},
-                    {"message": "Mudar template do site", "function": self._configure_site_template},
+                    {"message": "Criar post no site",
+                        "function": self._create_site_post},
+                    {"message": "Biblioteca de Mídias",
+                        "function": self._media_library_menu},
+                    {"message": "Ver estatísticas do site",
+                        "function": self._show_site_analytics},
+                    {"message": "Mudar template do site",
+                        "function": self._configure_site_template},
                 ]
             )
 
             if self.logged_user.username == self.selected_site.owner.username:
-                options.extend([{"message": "Adicionar Gerente", "function": self._add_manager}])
+                options.extend(
+                    [{"message": "Adicionar Gerente", "function": self._add_manager}])
 
         def display_title():
             template = build_site_template(
                 site=self.selected_site,
-                post_repo=AppContext().post_repo, # singleton
-                analytics_repo=AppContext().analytics_repo, # singleton
+                post_repo=AppContext().post_repo,  # singleton
+                analytics_repo=AppContext().analytics_repo,  # singleton
             )
             template.display()
 
@@ -64,7 +69,8 @@ class SiteMenu(AbstractMenu):
     def _create_site_post(self):
         try:
             facade = PostManagementFacade(AppContext())
-            post = facade.create_and_register_post(self.selected_site, self.logged_user)
+            post = facade.create_and_register_post(
+                self.selected_site, self.logged_user)
             print("\nPost criado com sucesso. Clique Enter para voltar ao menu.")
             input()
 
@@ -72,10 +78,9 @@ class SiteMenu(AbstractMenu):
             print(f"\nErro na criação do post: {e}")
             input("Clique Enter para voltar ao menu.")
 
-
     def _add_manager(self):
         print("Selecione um usuário para ser gerente da página:")
-        
+
         context = AppContext()
         users = context.permission_repo.get_not_managers(
             self.selected_site, context.user_repo
@@ -84,7 +89,8 @@ class SiteMenu(AbstractMenu):
             print(f"{i + 1}. {user.username} ({user.email})")
         print("0. Voltar")
 
-        selected_indexes = input("\nDigite os números separados por vírgula (ex: 1,3): ").split(",")
+        selected_indexes = input(
+            "\nDigite os números separados por vírgula (ex: 1,3): ").split(",")
 
         for idx in selected_indexes:
             idx = idx.strip()
@@ -97,7 +103,8 @@ class SiteMenu(AbstractMenu):
 
             if 1 <= n <= len(users):
                 user = users[n - 1]
-                print(f"Permissão de gerência dada ao usuário {user.username}.")
+                print(
+                    f"Permissão de gerência dada ao usuário {user.username}.")
                 context.permission_repo.grant_permission(
                     Permission(user=user, site=self.selected_site)
                 )
@@ -106,7 +113,7 @@ class SiteMenu(AbstractMenu):
 
     def _show_site_analytics(self):
         site = self.selected_site
-        analytics_repo = AppContext().analytics_repo # singleton
+        analytics_repo = AppContext().analytics_repo  # singleton
 
         accesses = analytics_repo.get_site_accesses(site.id)
         post_creations = analytics_repo.get_site_post_creation_count(site.id)
@@ -129,7 +136,8 @@ class SiteMenu(AbstractMenu):
         input("\nClique Enter para voltar ao Menu.")
 
     def _configure_site_template(self):
-        new_template = select_enum(SiteTemplateType, "Escolha o layout de apresentação do site:")
+        new_template = select_enum(
+            SiteTemplateType, "Escolha o layout de apresentação do site:")
         if new_template:
             self.selected_site.template = new_template
             print(f"Template atualizado para: {new_template.value}.", end=" ")
@@ -138,7 +146,8 @@ class SiteMenu(AbstractMenu):
         input("Clique enter para voltar ao menu.")
 
     def _select_post(self):
-        posts: list[Post] = AppContext().post_repo.get_site_posts(self.selected_site)
+        posts: list[Post] = AppContext(
+        ).post_repo.get_site_posts(self.selected_site)
 
         def execute_for_option(selected_post: Post):
             AppContext().analytics_repo.log(
@@ -150,7 +159,8 @@ class SiteMenu(AbstractMenu):
                 )
             )
             # construtor simplificado
-            PostMenu(self.logged_user, self.selected_site, selected_post).show()
+            PostMenu(self.logged_user, self.selected_site,
+                     selected_post).show()
 
         SiteMenu.prompt_generic(
             posts, "Posts do site", execute_for_option, lambda m: m.get_default_title()
